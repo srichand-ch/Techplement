ff0c16bacf7a05e99d6100c25fb7e6a2b826baa7
Ch. Sai Sri Chand <119035418+srichand-ch@users.noreply.github.com>
2024-05-08 11:18:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ function App() {
 
   const fetchQuoteOfTheDay = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/quote');
+      // const response = await fetch('http://localhost:5000/api/quote');
+      const response = await fetch('https://only-backend-1.onrender.com/api/quote');
       // const response = await fetch('https://techplement-orfj.onrender.com/api/quote');
       // const response = await fetch('${config.url}/quote');
       const data = await response.json();
@@ -37,7 +38,8 @@ function App() {
 
   const searchQuotes = async () => {
     try {
-      const response = await fetch(`http://localhost:5000/api/quotes/${searchTerm}`);
+      // const response = await fetch(`http://localhost:5000/api/quotes/${searchTerm}`);
+      const response = await fetch(`https://only-backend-1.onrender.com/api/quotes/${searchTerm}`);
       // const response = await fetch(`https://techplement-orfj.onrender.com/api/quotes/${searchTerm}`);
       const data = await response.json();
       console.log('Response data:', data);
